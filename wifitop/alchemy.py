3ce756ce6b6c09283c4472af1d71f0290c44d858
Marcus <marcus@hardt-it.de>
2020-10-19 21:36:46 +0200

    sqlalchemy stuff

---
+++ wifitop/alchemy.py
@@ -0,0 +1,25 @@
+'''information about one cell'''
+
+# vim: foldmethod=indent : tw=100
+#
+# This code is distributed under the MIT License
+#
+# pylint: disable=invalid-name, superfluous-parens
+# pylint: disable=logging-not-lazy, logging-format-interpolation
+
+import logging
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+
+import wifitop.logsetup
+
+logger  = logging.getLogger(__name__)
+
+engine  = create_engine('sqlite:///wifitop.db', echo = True)
+Session = sessionmaker(bind = engine)
+session = Session()
+
+Base    = declarative_base()
