2f3515318bf574acf383c2e5a6edb648316897d3
Marcus <marcus@hardt-it.de>
2021-03-07 21:35:28 +0100

    fix some preprocessing bugs

--- update-oui.sh
+++ update-oui.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
+#cat oui.snippet | \
 wget -q http://standards-oui.ieee.org/oui.txt -O -  |\
     grep hex |\
     sed s/\(hex\)// |\
-    sed s/Co.//gi |\
     sed s/ltd//gi |\
     sed s/inc//gi|\
     sed s/b.v.//gi |\
     sed s/llc//gi |\
     sed s/"LLC, a Lenovo pany"//gi |\
-    sed s/p.//gi |\
     sed s/gmbh//gi |\
     sed s/"+ KG"//gi |\
     sed s/\ [,.]// |\
@@ -17,4 +16,5 @@ wget -q http://standards-oui.ieee.org/oui.txt -O -  |\
     sed s/" \."//g |\
     sed s/[,.]\ *//  |\
     sed s/-/:/ |\
-    sed s/-/:/ >> oui.processed
+    sed s/-/:/ > oui.processed
+    #sed s/p.//gi |\
